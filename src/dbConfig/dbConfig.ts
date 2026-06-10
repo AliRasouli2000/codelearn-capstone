3c6e937a9b60f790468ef117178b09be7202de70
alirasouli <ali.rasouli2080@yahoo.com>
2026-06-10 00:52:18 -0700

    Fix duplicate mongoose listeners in db connect

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,17 +2,21 @@ import mongoose from "mongoose";
 
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGO_URI!)
+        await mongoose.connect(process.env.MONGO_URI!)
         const connection = mongoose.connection;
 
-        connection.on("connected", () => {
-            console.log("MongoDB connected successfully");
-        })
+        if (connection.listenerCount("connected") === 0) {
+            connection.on("connected", () => {
+                console.log("MongoDB connected successfully");
+            })
+        }
 
-        connection.on("error", (err) => {
-            console.log("MongoDB connection error, Please make sure MongoDB is running. " + err);
-            process.exit();
-        })
+        if (connection.listenerCount("error") === 0) {
+            connection.on("error", (err) => {
+                console.log("MongoDB connection error, Please make sure MongoDB is running. " + err);
+                process.exit();
+            })
+        }
 
     } catch (err) {
         console.log("Something went wrong!");
